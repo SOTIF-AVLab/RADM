9efab8e04d4d523c5962709f8ae76e73f1852f83
yangkaia123 <52814283+yangkaia123@users.noreply.github.com>
2023-02-24 16:15:49 +0800

    sss
    sss

---
+++ SUMO/src/test.py
@@ -0,0 +1,7 @@
+intersection environment defined by intersection_env.py
+
+
+run iqn: python train.py
+
+Note: there are several networks, i.e., dqn, iqn, ensemble iqn 
+        but the current evironmen is only suitable for iqn. The corresponding parameters for networks selection is fixed.
